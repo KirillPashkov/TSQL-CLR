9d57025e900f271e181d59b103ec899ef2eb8fa4
Kirill Pashkov <KirillPashkov@yandex.ru>
2017-12-06 16:06:42 +0300

    Create CreateFunction.sql

---
+++ CreateFunction.sql
@@ -0,0 +1,32 @@
+# To add assembly to SQL Server use:
+use [MyDB]
+go
+EXEC sp_changedbowner 'sa'
+ALTER DATABASE [MyDB] SET trustworthy ON
+go
+sp_configure 'clr enabled', 1
+go
+reconfigure
+go
+CREATE ASSEMBLY CLR_PKM FROM 'C:\CLR\CLR_PKM.dll' WITH PERMISSION_SET = UNSAFE;
+go
+CREATE FUNCTION [dbo].RegExSimpleMatch(@pattern [nvarchar](max), @matchString [nvarchar](max))
+RETURNS BIT
+EXTERNAL NAME CLR_PKM.Regex_PKM.RegExSimpleMatch
+go
+CREATE FUNCTION [dbo].StringTrim(@pattern [nvarchar](max), @charTrim [nvarchar](max))
+RETURNS NVARCHAR(MAX)
+EXTERNAL NAME CLR_PKM.StringTrim_PKM.StringTrim
+go
+CREATE FUNCTION [dbo].StringTrimStart(@pattern [nvarchar](max), @charTrim [nvarchar](max))
+RETURNS NVARCHAR(MAX)
+EXTERNAL NAME CLR_PKM.StringTrim_PKM.StringTrimStart
+go
+CREATE FUNCTION [dbo].StringTrimEnd(@pattern [nvarchar](max), @charTrim [nvarchar](max))
+RETURNS NVARCHAR(MAX)
+EXTERNAL NAME CLR_PKM.StringTrim_PKM.StringTrimEnd
+go
+# Where in CLR_PKM.Regex_PKM.RegExSimpleMatch:
+# CLR_PKM - Assembly name;
+# Regex_PKM - Public class in DLL;
+# RegExSimpleMatch - Function name in DLL.
